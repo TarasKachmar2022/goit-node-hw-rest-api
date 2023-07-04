2b0709b44493ae05be93b37957eb7ee8c731bd3c
Taras Kachmar <kachmar.taras@gmail.com>
2023-07-04 14:13:00 +0300

    add fs functions

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,61 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
+const path = require("path");
+const { nanoid } = require("nanoid");
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, "db", "contacts.json");
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => {
+  const contacts = await fs.readFile(contactsPath, "utf-8");
+  return JSON.parse(contacts);
+};
 
-const removeContact = async (contactId) => {}
+async function writeContacts(contacts) {
+  return await fs.writeFile(
+    contactsPath,
+    JSON.stringify(contacts, null, 2),
+    "utf-8"
+  );
+}
 
-const addContact = async (body) => {}
+const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+  const contact = contacts.find((contact) => contact.id === contactId);
+  return contact || null;
+};
 
-const updateContact = async (contactId, body) => {}
+const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+  const contactIndex = contacts.findIndex(
+    (contact) => contact.id === contactId
+  );
+  if (contactIndex === -1) {
+    return null;
+  }
+  const removedContact = contacts.splice(contactIndex, 1);
+  await writeContacts(contacts);
+  return removedContact;
+};
+
+const addContact = async ({ name, email, phone }) => {
+  const contacts = await listContacts();
+  const newContact = { name, email, phone, id: nanoid() };
+  contacts.push(newContact);
+  await writeContacts(contacts);
+  return newContact;
+};
+
+const updateContact = async (contactId, { name, email, phone }) => {
+  const contacts = await listContacts();
+  const contactIndex = contacts.findIndex(
+    (contact) => contact.id === contactId
+  );
+  if (contactIndex === -1) {
+    return null;
+  }
+  contacts[contactIndex] = { contactId, ...name, ...email, ...phone };
+  writeContacts(contacts[contactIndex]);
+  return contacts[contactIndex];
+};
 
 module.exports = {
   listContacts,
@@ -16,4 +63,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
