168a3c869d7400c6e9ab14459ab71031ca8fcfe8
Taras Kachmar <kachmar.taras@gmail.com>
2023-07-10 14:33:40 +0300

    first connect seccess

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const mongoose = require("mongoose");
 const app = require("./app");
-const { DB_HOST, PORT = 6000 } = process.env;
+// const { DB_HOST, PORT = 6000 } = process.env;
 
+DB_HOST =
+  "mongodb+srv://Taras:6eHR66NE5mwcHeH0@cluster0.dvayzso.mongodb.net/db-contacts?retryWrites=true&w=majority";
 mongoose.set("strictQuery", true);
+PORT = 6000;
 
 mongoose
   .connect(DB_HOST)
