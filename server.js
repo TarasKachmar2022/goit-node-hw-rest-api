84806f8e25a0ca825959ddaf88f0e78296d4930b
Taras Kachmar <kachmar.taras@gmail.com>
2023-07-10 15:32:32 +0300

    add processenv

--- server.js
+++ server.js
@@ -1,11 +1,6 @@
 const mongoose = require("mongoose");
 const app = require("./app");
-// const { DB_HOST, PORT = 6000 } = process.env;
-
-DB_HOST =
-  "mongodb+srv://Taras:6eHR66NE5mwcHeH0@cluster0.dvayzso.mongodb.net/db-contacts?retryWrites=true&w=majority";
-mongoose.set("strictQuery", true);
-PORT = 6000;
+const { DB_HOST, PORT = 6000 } = process.env;
 
 mongoose
   .connect(DB_HOST)
