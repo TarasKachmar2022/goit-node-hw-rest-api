b94159be5276caeb03232faf0efdd1b9b3a363de
Taras Kachmar <kachmar.taras@gmail.com>
2023-07-20 03:02:24 +0300

    add upload

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,9 +1,11 @@
 const isValidId = require("./isValidId");
 const validateBody = require("./validateBody");
 const authentificate = require("./authentificate");
+const upload = require("./upload");
 
 module.exports = {
   isValidId,
   validateBody,
   authentificate,
+  upload,
 };
